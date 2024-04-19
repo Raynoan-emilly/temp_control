a5bda2ca58c26de98cda1f5ca59956ee677f3ae5
Raynoan-emilly <raynoan.emilly@estudante.ufcg.edu.br>
2024-04-19 15:48:19 -0300

    add pins to the project

--- ControlTemp/ControlTemp.ino
+++ ControlTemp/ControlTemp.ino
@@ -3,17 +3,21 @@ Project: Measure the internal temperature of an airfreyer using an infrared temp
 Data: 04/17/2024
 Authors:
 -Flaviano Medeiros da Silva Júnior
--
+-Raynoan Emilly Silva Batista (raynoan.batista@ee.ufcg.edu.br)
 -
  ****************************************************/
-
+#include <Arduino.h>
+#include <WiFi.h>
 #include <Adafruit_MLX90614.h>
+//All the pins can be changed
+#define PIN_SENSOR 5     /*PIN SENSOR*/
+#define UP_BUTTON 14     /*Up temperature*/
+#define DOWN_BUTTON 19   /*Down  temperature*/
+#define PIN_SWICH 32     /*Swich*/
+
 
 
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
-
-void setup() {
-  Serial.begin(9600);
 
   /*----Start sensor!----*/
   void init_sensor(){
@@ -26,17 +30,27 @@ void setup() {
       };
   }
 
-  /*------Read Ambient Temperature------*/
-  float getTemperature(){
-    float TemperatureAmbient = float(mlx.readAmbientTempC());
-    return (TemperatureAmbient);
-  }
-
-  /*------Switch control------*/
-  void controlSwitch(){
+    /*------Switch control------*/
+  void controlSwitch(bool state ){
     float chosen_temperature;
     ........................
+    digitalWrite(PIN_SWICH, state);
   }
+
+void setup() {
+
+  Serial.begin(115200);
+
+      pinMode(PIN_SWICH, OUTPUT);
+      pinMode(UP_BUTTON, INPUT_PULLUP);
+      pinMode(DOWN_BUTTON, INPUT_PULLUP);
+
+    /*------Read Ambient Temperature------*/
+    float getTemperature(){
+     float TemperatureAmbient = float(mlx.readAmbientTempC());
+      return (TemperatureAmbient);
+   }
+
 }
 
 void loop() {
